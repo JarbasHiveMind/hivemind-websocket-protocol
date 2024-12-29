dc6bdf262da582d80a1a82c7a325e6eef1ae65ed
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-12-29 19:16:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ def required(requirements_file):
                 if pkg.strip() and not pkg.startswith("#")]
 
 
+PLUGIN_ENTRY_POINT = 'hivemind-websocket-plugin=hivemind_websocket_protocol:HiveMindWebsocketProtocol'
 
 setup(
     name='hivemind-websocket-protocol',
@@ -48,6 +49,7 @@ setup(
     license='Apache-2.0',
     author='jarbasAi',
     install_requires=required("requirements.txt"),
+    entry_points={'hivemind.network.protocol': PLUGIN_ENTRY_POINT},
     author_email='jarbasai@mailfence.com',
     description='websocket network protocol for hivemind-core'
 )
